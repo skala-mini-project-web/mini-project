85adc01d44fb928c01635547600cf10ac1aae636
sonseohy <sonseoh223@gmail.com>
2026-09-03 01:44:41 +0900

    chore: 초기 백엔드 세팅

---
+++ backend/src/main/java/com/crosschecklab/CrosschecklabApplication.java
@@ -0,0 +1,13 @@
+package com.crosschecklab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrosschecklabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrosschecklabApplication.class, args);
+	}
+
+}
